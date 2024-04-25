55de45f96669cfcd15b64b759ca4ce01503fb3ed
Arpana Sharma <99793653+Arpana-Sharma@users.noreply.github.com>
2024-04-25 22:02:47 +0530

    Delete proj directory

--- proj/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.real_time_view, name="main"),
-    path("real_time/", views.real_time_view, name="real_time"),
-    path("result/", views.result_view, name="result"),
-    path("login/", views.login_view, name="login")
-]
